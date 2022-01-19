02416d921d39e3003a67ac8650cd5c4644f412bd
hemji1912 <86617052+hemji1912@users.noreply.github.com>
2022-01-19 19:38:20 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/epubebookapp/MainActivity.java
+++ app/src/main/java/com/example/epubebookapp/MainActivity.java
@@ -11,4 +11,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // runtime permission function by hemant vishwakarma
+      private void runtimepermission()
+    {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response)
+                    {
+                        display();
+                     }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response)
+                    {
+
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token)
+                    {
+                    token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+      // pdf reader function by hemant vishwakarma
+     public ArrayList findpdf(File file)
+    {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for(File singlefile:files)
+        {
+            if(singlefile.isDirectory() && !singlefile.isHidden())
+            {
+                arrayList.addAll(findpdf(singlefile));
+            }
+            else
+            {
+                if(singlefile.getName().endsWith(".pdf"))
+                {
+                    arrayList.add(singlefile);
+                }
+            }
+        }
+        return  arrayList;
+    }
+    //display pdf files by hemant vishwakarma
+    public void display()
+    {
+        Filename = new ArrayList<>();
+        Filename.addAll(findpdf(Environment.getExternalStorageDirectory()));
+        adapter = new adapter(this,Filename);
+        //set adapter in recycler view
+    }
 }
